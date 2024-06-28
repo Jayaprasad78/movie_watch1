555cb3be3d0b81ad0333e3753346cce742346cd0
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-06-28 13:24:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,18 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const cors = require('cors');
-server.use(cors());
+
+// Use CORS middleware to allow requests from specified origin
+server.use(cors({
+  origin: ["https://deploy-mern-frontend.vercel.app"],
+  methods: ["POST", "GET","DELETE","PUT"],
+  credentials: true
+}));
+
 const middlewares = jsonServer.defaults({
   static: "./build"
 });
+
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
@@ -14,7 +22,9 @@ server.use(
     "/api/*": "/$1"
   })
 );
+
 server.use(router);
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
