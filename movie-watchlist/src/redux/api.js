881946c9b73b0b4bd11d0aa19b87700be4d39076
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-06-28 13:36:27 +0530

    Update api.js

--- movie-watchlist/src/redux/api.js
+++ movie-watchlist/src/redux/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // JSON Server URL
+  baseURL: 'https://movie-watch2.vercel.app', // JSON Server URL
 });
 
 export default api;
